8045a2fc2c89742722f9d27ac2c376823c10a526
Gustavo Fontana Bertoldi <gustavobertoldi20@gmail.com>
2024-12-19 11:23:22 -0300

    creating a binary tree, part 1

---
+++ arquivo_exercícios/aulas_C/conteudos_curso_C/Binary_tree.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+    Binary Tree is a non-linear and hierarchical data structure where 
+    each node has at most two children referred to as the left child and 
+    the right child.  The topmost node in a binary tree is called 
+    the root, and the bottom-most nodes are called leaves. 
+
+    
+*/
